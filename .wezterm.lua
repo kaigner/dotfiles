830a6108f3cf37c28dd06c11ce5c047ee72dab92
Klaus Aigner <kla@space.net>
2024-07-22 09:11:23 +0200

    chg: bring back @ and | on mac

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,5 +10,7 @@ config.font_size = 16
 config.enable_tab_bar = false
 --  config.window_decorations = "TITLE  RESIZE"
 
+-- bring @ and | back to mac keyboards
+config.send_composed_key_when_left_alt_is_pressed = true
 
 return config
